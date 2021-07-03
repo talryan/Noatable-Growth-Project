4e0d795b1cef0e547cf1b25f83deaa1581399343
talryan <nataliamryan@gmail.com>
2021-07-03 12:03:26 -0700

    Modal 2 Done

--- testProject/src/main.js
+++ testProject/src/main.js
@@ -1,1 +1,44 @@
 
+  document.addEventListener("DOMContentLoaded", () => {
+  modalA()
+}) 
+
+function modalA(){
+  addRadioToModalB();
+}
+
+
+
+function addRadioToModalA(){
+  let modalA = document.querySelector(".modal-a");
+  modalA.innerHTML +=
+  `
+  <form> 
+  <h5> We want to make sure we offer relevant products</h5> 
+  <Strong> Do you have any children? </Strong><br>
+  <label> Yes </label>
+  <input  type="radio" id="modalAYes" name = "yes" value= "true"><br> 
+  <label> No </label> 
+  <input  type="radio" id="modalANo" name = "no" value= "false"><br>
+  <input type="submit" class="btn" value="Submit">
+  </form>
+
+  `;
+}
+
+function addRadioToModalB(){
+  let modalB = document.querySelector(".modal-b");
+  modalB.innerHTML +=
+  `
+  <form> 
+  <h5> Last Question</h5> 
+  <Strong> Do you have any pets?</Strong><br>
+  <label> Yes </label>
+  <input  type="radio" id="modalBYes" name = "yes" value= "true"><br> 
+  <label> No </label> 
+  <input  type="radio" id="modalBNo" name = "no" value= "false"><br>
+  <input type="submit" class="btn" value="Submit">
+  </form>
+
+  `;
+}
