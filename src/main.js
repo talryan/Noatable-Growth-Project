924ed9c8cc5b875a23e956db87382a8fdd8d7e86
talryan <nataliamryan@gmail.com>
2021-10-04 12:33:24 -0700

    Added Questions.

--- src/main.js
+++ src/main.js
@@ -68,6 +68,137 @@ function validateChildrenQuestion(e){
     }
   }
     if (isValid){
+      saveResponse(answer,subscriptionModal);
+    }
+    else {
+      modalError();
+    }
+    
+}
+
+
+function subscriptionModal(){
+  let modal = document.querySelector(".modal-body");
+ 
+  modal.innerHTML =
+  `
+  <form> 
+  <div class="modal-content"> 
+  <Strong> Would you like products delivered to you the same time each month? </Strong><br>
+  <div class="radio-strong">
+  <strong class="radio-label"> Yes </strong>
+  <strong class="radio-label"> No </strong> 
+  </div>
+  <div class="radio-buttons">
+  <span class="faux-radio" data-radio="yes" data-radio-name="radioSubscription"></span>
+  <span class="faux-radio" data-radio="no" data-radio-name="radioSubscription"></span>
+  <input  type="radio" class="radio" id="subscriptionModalYes" name="radioSubscription" value="yes">
+  <input  type="radio" class="radio" id="subscriptionModalNo" name="radioSubscription" value="no">
+
+  </div>
+  </div>
+  <input type="submit" class="btn" value="Submit">
+  </form>
+  `;
+ 
+  modal.addEventListener('submit',validateSubscriptionQuestion)
+  document.querySelectorAll('span[data-radio-name="radioSubscription"]')
+  .forEach(element => element.addEventListener('click',function(){
+    console.log(this.getAttribute('data-radio'))
+    document.querySelectorAll('span[data-radio-name="radioSubscription"]')
+    .forEach(e => e.classList.remove("selected"))
+    this.classList.add("selected")
+    const getAttribute = this.getAttribute('data-radio')
+    document.querySelectorAll(`input[name="radioSubscription"]`)
+    .forEach(x => x.removeAttribute("checked"))
+    document.querySelector(`input[name="radioSubscription"][value="${getAttribute}"]`)
+    .setAttribute("checked", "checked")
+  }));
+
+}
+
+function validateSubscriptionQuestion(e){
+  e.preventDefault();
+  let modal = document.querySelector(".modal-body");
+  modal.removeEventListener('submit',validateSubscriptionQuestion)
+  const radioSubscription =document.getElementsByName('radioSubscription')
+  let isValid = false
+  let answer = {}
+  for (var i = 0, length = radioSubscription.length; i < length; i++) {
+    if (radioSubscription[i].checked) {
+      answer = {hasSubscription:radioSubscription[i].value === "yes"}
+        radioInputA= radioSubscription[i].value
+        isValid = true
+        break
+    }
+  }
+  console.log(saveResponse);
+    if (isValid){
+      saveResponse(answer,refillModal);
+    }
+    else {
+      modalError();
+    }
+
+    
+}
+function refillModal(){
+  let modal = document.querySelector(".modal-body");
+ 
+  modal.innerHTML =
+  `
+  <form> 
+  <div class="modal-content"> 
+  <Strong> Would you participate in our recycling program? </Strong><br>
+  <label>We will provide sustainable containers and refill them!</label>
+  <div class="radio-strong">
+  <strong class="radio-label"> Yes </strong>
+  <strong class="radio-label"> No </strong> 
+  </div>
+  <div class="radio-buttons">
+  <span class="faux-radio" data-radio="yes" data-radio-name="radioRefill"></span>
+  <span class="faux-radio" data-radio="no" data-radio-name="radioRefill"></span>
+  <input  type="radio" class="radio" id="refillModalYes" name="radioRefill" value="yes">
+  <input  type="radio" class="radio" id="refillModalNo" name="radioRefill" value="no">
+
+  </div>
+  </div>
+  <input type="submit" class="btn" value="Submit">
+  </form>
+  `;
+ 
+  modal.addEventListener('submit',validateRefillQuestion)
+  document.querySelectorAll('span[data-radio-name="radioRefill"]')
+  .forEach(element => element.addEventListener('click',function(){
+    console.log(this.getAttribute('data-radio'))
+    document.querySelectorAll('span[data-radio-name="radioRefill"]')
+    .forEach(e => e.classList.remove("selected"))
+    this.classList.add("selected")
+    const getAttribute = this.getAttribute('data-radio')
+    document.querySelectorAll(`input[name="radioRefill"]`)
+    .forEach(x => x.removeAttribute("checked"))
+    document.querySelector(`input[name="radioRefill"][value="${getAttribute}"]`)
+    .setAttribute("checked", "checked")
+  }));
+
+}
+
+function validateRefillQuestion(e){
+  e.preventDefault();
+  let modal = document.querySelector(".modal-body");
+  modal.removeEventListener('submit',validateRefillQuestion)
+  const radioRefill =document.getElementsByName('radioRefill')
+  let isValid = false
+  let answer = {}
+  for (var i = 0, length = radioRefill.length; i < length; i++) {
+    if (radioRefill[i].checked) {
+      answer = {hasRefill:radioRefill[i].value === "yes"}
+        radioInputA= radioRefill[i].value
+        isValid = true
+        break
+    }
+  }
+    if (isValid){
       saveResponse(answer,petModal);
     }
     else {
